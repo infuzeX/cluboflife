a2080f631279400dc783db9aece614cb93a31c83
charanpy <durgicharan81@gmail.com>
2021-09-16 21:23:18 +0530

    fixed minor bugs

---
+++ public/assets/js/user.js
@@ -0,0 +1,29 @@
+const username = document.querySelector('.username');
+const getMe = async () => {
+  try {
+    const res = await fetch('/api/v1/users/me');
+    const user = await res.json();
+    if (user.status === 'fail' || user.status === 'error') {
+      throw new Error(user?.message);
+    }
+    if (username) {
+      username.textContent = user?.data?.user?.name;
+      username.style.textTransform = 'capitalize';
+    }
+
+    return user?.data?.user;
+  } catch (error) {
+    tempAlert(error?.message || 'Something went wrong', 5000, true);
+    return false;
+  }
+};
+
+document.querySelector('.logout')?.addEventListener('click', () => {
+  fetch('/api/v1/auth/logout')
+    .then((res) => {
+      if (res.redirected) {
+        window.location = res.url;
+      }
+    })
+    .catch((e) => console.log(e));
+});
